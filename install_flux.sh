954e2bb24d7cadeebad8fcd36e00571c51628965
animanathome <animanatwork@me.com>
2024-08-25 19:29:07 -0700

    adding workflows

--- install_flux.sh
+++ install_flux.sh
@@ -32,12 +32,38 @@ huggingface-cli download openai/clip-vit-large-patch14 model.safetensors --local
 # XLabs-AI
 huggingface-cli download XLabs-AI/flux-ip-adapter flux-ip-adapter.safetensors --local-dir ComfyUI/models/xlabs/ipadapters/
 
-huggingface-cli download XLabs-AI/flux-controlnet-collections flux-canny-controlnet_v2.safetensors --local-dir ComfyUI/models/xlabs/controlnets/
-huggingface-cli download XLabs-AI/flux-controlnet-collections flux-depth-controlnet_v2.safetensors --local-dir ComfyUI/models/xlabs/controlnets/
-huggingface-cli download XLabs-AI/flux-controlnet-collections flux-canny-controlnet.safetensors --local-dir ComfyUI/models/xlabs/controlnets/
-huggingface-cli download XLabs-AI/flux-controlnet-collections flux-depth-controlnet.safetensors --local-dir ComfyUI/models/xlabs/controlnets/
-huggingface-cli download XLabs-AI/flux-controlnet-collections flux-hed-controlnet.safetensors --local-dir ComfyUI/models/xlabs/controlnets/
-huggingface-cli download XLabs-AI/flux-controlnet-canny controlnet.safetensors --local-dir ComfyUI/models/xlabs/controlnets/
+huggingface-cli download XLabs-AI/flux-ip-adapter ip_adapter_workflow.json --local-dir ComfyUI/user/default/workflows
 
+# canny
+huggingface-cli download XLabs-AI/flux-controlnet-canny controlnet.safetensors --local-dir ComfyUI/models/xlabs/controlnets/
+huggingface-cli download XLabs-AI/flux-controlnet-collections flux-canny-controlnet.safetensors --local-dir ComfyUI/models/xlabs/controlnets/
+huggingface-cli download XLabs-AI/flux-controlnet-collections flux-canny-controlnet_v2.safetensors --local-dir ComfyUI/models/xlabs/controlnets/
+huggingface-cli download XLabs-AI/flux-controlnet-canny-v3 flux-canny-controlnet-v3.safetensors --local-dir ComfyUI/models/xlabs/controlnets/
+
+huggingface-cli download XLabs-AI/flux-controlnet-canny-v3 flux-controlnet-canny-v3-workflow.json --local-dir ComfyUI/user/default/workflows
+
+# depth
+huggingface-cli download XLabs-AI/flux-controlnet-collections flux-depth-controlnet.safetensors --local-dir ComfyUI/models/xlabs/controlnets/
+huggingface-cli download XLabs-AI/flux-controlnet-collections flux-depth-controlnet_v2.safetensors --local-dir ComfyUI/models/xlabs/controlnets/
+huggingface-cli download XLabs-AI/flux-controlnet-depth-v3 flux-depth-controlnet-v3.safetensors --local-dir ComfyUI/models/xlabs/controlnets/
+
+huggingface-cli download XLabs-AI/flux-controlnet-depth-v3 flux-controlnet-depth-v3-workflow.json --local-dir ComfyUI/user/default/workflows
+
+
+# hed
+huggingface-cli download XLabs-AI/flux-controlnet-collections flux-hed-controlnet.safetensors --local-dir ComfyUI/models/xlabs/controlnets/
+huggingface-cli download XLabs-AI/flux-controlnet-hed-v3 flux-hed-controlnet-v3.safetensors --local-dir ComfyUI/models/xlabs/controlnets/
+
+huggingface-cli download XLabs-AI/flux-controlnet-hed-v3 flux-controlnet-hed-v3-workflow.json --local-dir ComfyUI/user/default/workflows
+
+# loras
 huggingface-cli download XLabs-AI/flux-RealismLora lora.safetensors --local-dir ComfyUI/models/xlabs/loras
+huggingface-cli download XLabs-AI/flux-lora-collection disney_lora.safetensors --local-dir ComfyUI/models/xlabs/loras
 huggingface-cli download XLabs-AI/flux-lora-collection furry_lora.safetensors --local-dir ComfyUI/models/xlabs/loras
+huggingface-cli download XLabs-AI/flux-lora-collection realism_lora.safetensors --local-dir ComfyUI/models/xlabs/loras
+huggingface-cli download XLabs-AI/flux-lora-collection scenery_lora.safetensors --local-dir ComfyUI/models/xlabs/loras
+huggingface-cli download XLabs-AI/flux-lora-collection art_lora.safetensors --local-dir ComfyUI/models/xlabs/loras
+
+
+
+

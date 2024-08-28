09b193d21b8cd6bbef5bafee2ab6e811b2024672
animanathome <animanatwork@me.com>
2024-08-28 14:53:50 -0700

    cleanup

--- scripts/queue_utils.py
+++ scripts/queue_utils.py
@@ -8,11 +8,32 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 max_retries = 100
+wait_time = 1
 server_address = "http://127.0.0.1:8080"
 
 # this is where comfyUI stores its temporary files or images
 temp_directory = 'ComfyUI/temp/'
-output_directory = 'output'
+output_directory = 'ComfyUI/output/'
+dest_directory = 'output'
+
+
+def get_image_path(image_file, image_type):
+    source_directory = None
+    if image_type == 'temp':
+        source_directory = temp_directory
+    elif image_type == 'output':
+        source_directory = output_directory
+    else:
+        raise ValueError(f'Unknown image type {image_type}')
+
+    return os.path.join(source_directory, image_file)
+
+
+def move_file(source, destination):
+    os.makedirs(os.path.dirname(destination), exist_ok=True)
+
+    shutil.copy(source, destination)
+
 
 def queue_prompt(prompt, output_file):
     prompt_endpoint = f"{server_address}/prompt"
@@ -27,6 +48,7 @@ def queue_prompt(prompt, output_file):
     except requests.RequestException as e:
         logger.info(f"Error queuing workflow: {e}")
 
+
 def wait_for_image(prompt_id, output_file):
     for attempt in range(max_retries):
         try:
@@ -39,20 +61,24 @@ def wait_for_image(prompt_id, output_file):
 
                 if 'outputs' in task_history and len(task_history['outputs']) > 0:
                     output = task_history['outputs']
+                    logger.info(output)
 
                     for node in output.keys():
                         if 'images' in output[node]:
                             image_file = output[node]['images'][0]['filename']
-                            source_path = os.path.join(temp_directory, image_file)
-                            target_path = os.path.join(output_directory, output_file)
-                            shutil.copy(source_path, target_path)
+                            image_type = output[node]['images'][0]['type']
+
+                            logger.info(f'Copying over temp image: {image_file}')
+                            source_path = get_image_path(image_file, image_type)
+                            target_path = os.path.join(dest_directory, output_file)
+                            move_file(source_path, target_path)
                             logger.info(f"Image saved: {target_path}")
                             return
 
             logger.info(f"Waiting for image... (Attempt {attempt + 1}/{max_retries})")
-            time.sleep(1)
+            time.sleep(wait_time)
         except requests.RequestException as e:
             logger.info(f"Error checking history: {e}")
-            time.sleep(1)
+            time.sleep(wait_time)
 
     logger.error("Max retries reached. Image generation may have failed.")
